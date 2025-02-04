bd61705cd99be16b2fadcda38a64b104a95a90e5
Lukas Germerott <github@lukasgermerott.de>
2025-02-04 21:48:13 +0100

    feat: remove cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
-
 const nextConfig = {
   /* config options here */
 };
