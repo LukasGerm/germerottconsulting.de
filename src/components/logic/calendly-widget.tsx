c9bb41220769fec8828e183184f1d64191e2754c
Lukas Germerott <github@lukasgermerott.de>
2025-02-04 20:00:39 +0100

    fix: calendly

--- src/components/logic/calendly-widget.tsx
+++ src/components/logic/calendly-widget.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import styles from "./calendly-widget.module.css";
-import Script from "next/script";
+
+import { InlineWidget } from "react-calendly";
+import { useConsentGiven } from "./providers/consent-provider";
+
 export const CalendlyWidget = (props: { variant: "grey" | "white" }) => {
   const { variant } = props;
+
+  const consentGiven = useConsentGiven();
+
   return (
     <section
       className={cn(
@@ -18,15 +26,18 @@ export const CalendlyWidget = (props: { variant: "grey" | "white" }) => {
         <h2 className="font-bold text-4xl">Jetzt direkt Termin buchen</h2>
         <hr className="w-24 h-2 bg-primary mx-auto mt-4 mb-4 border-t-2" />
       </div>
-      <div
-        className={cn("calendly-inline-widget", styles["calendly-widget"])}
-        data-url="https://calendly.com/lukas-lukasgermerott/30min?background_color=ffffff&text_color=000000&primary_color=1e3a5f"
-      ></div>
-      <Script
-        type="text/javascript"
-        src="https://assets.calendly.com/assets/external/widget.js"
-        async
-      ></Script>
+      <div className="pb-36 w-full">
+        <InlineWidget
+          styles={{
+            width: "100%",
+            height: "1000px",
+          }}
+          url={
+            "https://calendly.com/lukas-lukasgermerott/30min?background_color=ffffff&text_color=000000&primary_color=1e3a5f" +
+            (consentGiven ? "&hide_gdpr_banner=1" : "")
+          }
+        />
+      </div>
     </section>
   );
 };
